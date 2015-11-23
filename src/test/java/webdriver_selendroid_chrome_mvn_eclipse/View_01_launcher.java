7991987bab49dda487fac63200dd786cca78b3b5
roudox <nicholasardila@hotmail.com>
2015-11-23 17:58:12 -0500

    updated uat link and selector elements for new env

--- src/test/java/webdriver_selendroid_chrome_mvn_eclipse/View_01_launcher.java
+++ src/test/java/webdriver_selendroid_chrome_mvn_eclipse/View_01_launcher.java
@@ -11,7 +11,7 @@ public class View_01_launcher extends BaseTest {
 	
 	@Before
 	  public void setUp() throws Exception {
-		super.setUp("http://stagemapi2.canadiantire.ca/");
+		super.setUp("http://autopartfinder.cantire.net/#/config/0022/en/00912383");
 		}
 
 	@Override
@@ -23,17 +23,19 @@ public class View_01_launcher extends BaseTest {
 	
 	@Test
 	public void startWebDriver(){	
-		driver.get(baseUrl + "/autopartfinder/#/config/0022/en/0050563FFFED");
+		driver.get(baseUrl);
 	    driver.findElement(By.cssSelector("span.launcher-button-text.ng-binding")).click();
 	    new Select(driver.findElement(By.id("catEN"))).selectByVisibleText("BATTERY");
 	    new Select(driver.findElement(By.id("year"))).selectByVisibleText("2003");
 	    new Select(driver.findElement(By.id("make"))).selectByVisibleText("ACURA");
 	    new Select(driver.findElement(By.id("model"))).selectByVisibleText("RSX");
 	    new Select(driver.findElement(By.id("engine"))).selectByVisibleText("4-1998 2.0L DOHC");
-	    driver.findElement(By.xpath("//button[@type='search']")).click();
-	    driver.findElement(By.cssSelector("div.inner-align")).click();
-	    driver.findElement(By.linkText("BACK")).click();
-	    driver.findElement(By.cssSelector("img.pull-left.center-icon")).click();	
+	    driver.findElement(By.cssSelector(".search-button")).click();
+	    driver.findElement(By.cssSelector("div.eye-icon")).click();
+	    driver.findElement(By.cssSelector("li.pdp-label.ng-isolate-scope.active")).click();
+	    driver.findElement(By.cssSelector("li.pdp-label.ng-isolate-scope")).click();
+	    driver.findElement(By.cssSelector(".pdp-close-button-content")).click();
+	    driver.findElement(By.cssSelector("img.pull-left.center-icon")).click();
 	}
 	
 
